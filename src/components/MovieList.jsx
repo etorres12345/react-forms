c565c3f8657e33384c2621338286b5d8e5c90568
etorres12345 <erik.torres1215@gmail.com>
2023-09-20 13:48:09 +0200

    done

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import moviesDataJSON from "../movies-data.json";
+import MovieCard from "./MovieCard";
+import AddMovie from "./AddMovie";
+import FilterMovies from "./FilteredMovies";
+
+function MovieList() {
+  const [movies, setMovies] = useState(moviesDataJSON);
+  const [moviesData, setMoviesData] = useState(moviesDataJSON);
+
+  const addNewMovie = (newMovie) => {
+    const updatedMovies = [...movies, newMovie];
+    const updatedMoviesData = [...moviesData, newMovie];
+    setMovies(updatedMovies);
+    setMoviesData(updatedMoviesData);
+  };
+
+  const filterMovieList = (str) => {
+    let filteredMovies;
+
+    if (str === "All") {
+      filteredMovies = moviesData;
+    } else {
+      filteredMovies = moviesData.filter((movie) => {
+        return movie.title[0].toLowerCase() === str.toLowerCase();
+      });
+    }
+
+    setMovies(filteredMovies);
+  };
+
+  return (
+    <div>
+      <FilterMovies FilterMovies={filterMovieList} />
+      <AddMovie addMovie={addNewMovie} />
+      {movies.map((movie) => {
+        return <MovieCard key={movie._id} movie={movie} />;
+      })}
+    </div>
+  );
+}
+
+export default MovieList;
